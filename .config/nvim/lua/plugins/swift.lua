4fe41eda7f85c41d627d85f83570af0280fe5d3b
Håkon Harnes <hakon@harnes.co>
2025-04-16 20:37:22 +0200

    feat: add treesitter support for swift

--- .config/nvim/lua/plugins/swift.lua
+++ .config/nvim/lua/plugins/swift.lua
@@ -22,4 +22,12 @@ return {
       })
     end,
   },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      if type(opts.ensure_installed) == "table" then
+        vim.list_extend(opts.ensure_installed, { "swift" })
+      end
+    end,
+  },
 }
